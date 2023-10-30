6583619015c6cd9973c2afd66d40840240422f49
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-30 23:58:33 +0530

    add new textfield to modal

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
+
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -36,7 +38,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseModal() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const Text('Add Expense'),
+      builder: (ctx) => const NewExpense(),
     );
   }
 
