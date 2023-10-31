36e1e6c108541d79fe2eb0f8a817e712bceefee7
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-11-01 00:49:10 +0530

    update modal

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -36,6 +36,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseModal() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
