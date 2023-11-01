e8643ce4afc8d9431214c8658113cf0b3731a42d
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-11-02 01:13:11 +0530

    add default styles

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,37 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 82, 59, 181),
+);
+
 void main() {
   runApp(
-     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.primary,
+          foregroundColor: kColorScheme.onPrimary,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.secondaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onPrimary,
+              ),
+            ),
+      ),
       home: const Expenses(),
     ),
   );
