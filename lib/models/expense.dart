9a8fff30297623320600cabd1d3cd3745b52d446
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-11-02 15:53:51 +0530

    add utility contructor ro expenses bucket

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -38,12 +38,16 @@ class Expense {
   }
 }
 
-class ExpenseBucket{
-
-  ExpenseBucket({ required this.category, required this.expenses});
+class ExpenseBucket {
+  ExpenseBucket({required this.category, required this.expenses});
 
   final Category category;
   final List<Expense> expenses;
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
 
   double get totalAmount {
     double sum = 0;
